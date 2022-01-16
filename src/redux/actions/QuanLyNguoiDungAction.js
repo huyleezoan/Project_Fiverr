fb6ffb174889f266784e4cca0a63825198ea9f5f
huydeptrai <ldmh2042002@gmail.com>
2022-01-16 10:28:48 +0700

    detail

--- src/redux/actions/QuanLyNguoiDungAction.js
+++ src/redux/actions/QuanLyNguoiDungAction.js
@@ -1,3 +1,12 @@
+import { http } from "../../util/setting";
+import { SET_THONG_TIN_CHI_TIET_NGUOI_DUNG } from "./../types/QuanLyNguoiDungType";
 
-
-
+export const getThongTinChiTietNguoiDung = (maNguoiDung) => {
+  return async (dispatch) => {
+    const result = await http.get(`/api/users/${maNguoiDung}`);
+    dispatch({
+      type: SET_THONG_TIN_CHI_TIET_NGUOI_DUNG,
+      detailPerson: result.data,
+    });
+  };
+};
